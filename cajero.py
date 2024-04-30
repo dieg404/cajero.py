01cc8fe855512c0c67bd34a0b8f2aae3f7ae2cae
dieg404 <158373396+dieg404@users.noreply.github.com>
2024-04-30 12:24:28 -0500

    1

--- cajero.py
+++ cajero.py
@@ -1,3 +1,26 @@
-print("Hola")
+def menu():
+    print("1. Consignar plata.")
+    print("2. Retirar plata.")
+    print("3. Consultar saldo.")
+    print("4. Salir")
 
-#esto es un comentario
+
+def inicio():
+    
+
+
+
+
+
+#Inicio codigo:
+
+cedula =[]
+password = []
+b = 1
+
+
+x1 = int(input("Ingrese cuantas cuentas desea registrar. "))
+while b <= x1:
+    cx = int(input(f"Digite la cuenta numero {b}"))
+    cedula.append(cx)
+    px = int(input(f"Digite la contrase;a de la cuenta nuemro {b}"))
